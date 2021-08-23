e2ef3af8120217d90199e4c53f8b06f7cc316897
aivnerrad <aivnerrad1993@gmail.com>
2021-08-23 19:32:27 -0400

    login routes complete

--- groovium/routes/index.js
+++ groovium/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 const { User } = require('../db/models');
 const bcrypt = require('bcryptjs');
 const { asyncHandler } = require('../utils');
+const { loginValidators, validationResult } = require('./validations')
 
 var router = express.Router();
 
@@ -34,5 +35,47 @@ router.post('/sign-up', asyncHandler(async(req, res) => {
 
   res.redirect('/users');
 }));
+// come back and make these better using regex
+router.get('/log-in', asyncHandler(async(req, res) => {
+  res.render('log-in');
+}))
 
+router.post('/log-in', loginValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      email,
+      password,
+    } = req.body;
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      // Attempt to get the user by their email address.
+      const user = await User.findOne({ where: { email } });
+
+      if (user !== null) {
+        // If the user exists then compare their password
+        // to the provided password.
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+        if (passwordMatch) {
+          // If the password hashes match, then login the user
+          // and redirect them to the default route.
+          // TODO Login the user.
+          return res.redirect('/users');
+        }
+      }
+
+      // Otherwise display an error message to the user.
+      errors.push('Login failed for the provided email address and password');
+    } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+
+    res.render('user-login', {
+      email,
+      errors
+    });
+  }));
 module.exports = router;
---
+++ groovium/routes/validations.js
@@ -0,0 +1,34 @@
+const { check, validationResult } = require("express-validator")
+const loginValidators = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 50 })
+    .withMessage('First Name must not be more than 50 characters long'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Last Name')
+    .isLength({ max: 50 })
+    .withMessage('Last Name must not be more than 50 characters long'),
+  check('emailAddress')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long'),
+  ];
+module.exports = {
+  loginValidators,
+  validationResult
+}
