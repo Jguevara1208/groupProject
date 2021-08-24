10931ceda17ef8280ce572adefe2b0cf49f310a6
aivnerrad <aivnerrad1993@gmail.com>
2021-08-24 19:07:12 -0400

    updated seeder files and finished topics list pug

--- groovium/routes/topics.js
+++ groovium/routes/topics.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require("../utils")
-const { Story, Topic } = require("../db/models");
+const { Topic } = require("../db/models");
 
 router.get('/', asyncHandler(async (req, res) => {
     const topics = await Topic.findAll()
