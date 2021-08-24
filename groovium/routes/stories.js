66632e9f63ce744492bb9f1998930e39f8e4f185
aivnerrad <aivnerrad1993@gmail.com>
2021-08-24 17:02:16 -0400

    more stories progress

--- groovium/routes/stories.js
+++ groovium/routes/stories.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { asyncHandler } = require("../utils")
 const { Story } = require("../db/models");
 const { User } = require('../db/models');
+const { Topic } = require('../db/models')
 
 router.get('/:storyId', asyncHandler(async (req, res) => {
   console.log("Are we there yet?")
@@ -10,8 +11,11 @@ router.get('/:storyId', asyncHandler(async (req, res) => {
   console.log(storyId)
   const story = await Story.findByPk(storyId)
   const user = await User.findByPk(story.userId)
-  console.log("THE USER --------> ", story)
-  res.render('stories', { story, user })
+  const topic = await Topic.findByPk(story.topicId)
+  // console.log("THE STORY --------> ", story)
+  // console.log("THE USER --------> ", user)
+  // console.log("THE TOPIC --------> ", topic)
+  res.render('stories', { story, user, topic })
 }));
 
 
