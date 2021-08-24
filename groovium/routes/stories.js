d1830b9d27e82aacd8177eaee64c22e38a1cdfa4
aivnerrad <aivnerrad1993@gmail.com>
2021-08-24 18:08:12 -0400

    working on the topics list page

--- groovium/routes/stories.js
+++ groovium/routes/stories.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require("../utils")
-const { Story } = require("../db/models");
-const { User } = require('../db/models');
-const { Topic } = require('../db/models')
+const { Story, User, Topic } = require("../db/models");
 
 router.get('/:storyId', asyncHandler(async (req, res) => {
-  console.log("Are we there yet?")
   const storyId = req.url.split("/")[1]
-  console.log(storyId)
   const story = await Story.findByPk(storyId)
   const user = await User.findByPk(story.userId)
   const topic = await Topic.findByPk(story.topicId)
@@ -17,6 +13,5 @@ router.get('/:storyId', asyncHandler(async (req, res) => {
   // console.log("THE TOPIC --------> ", topic)
   res.render('stories', { story, user, topic })
 }));
-
 
 module.exports = router;
--- groovium/routes/topics.js
+++ groovium/routes/topics.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require("../utils")
+const { Story, Topic } = require("../db/models");
 
 router.get('/', asyncHandler(async (req, res) => {
-    res.render('topics-list')
+    const topics = await Topic.findAll()
+    res.render('topics-list', { topics })
 }))
 
 module.exports = router;
