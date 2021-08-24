5746b77d969ddca35b6b05434aae43cd53948280
Peter Joh <johpeter@gmail.com>
2021-08-24 11:32:32 -0600

    working on routes

--- groovium/routes/index.js
+++ groovium/routes/index.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const csrf = require('csurf')
 const csrfProtection = csrf({ cookie: true });
 const { asyncHandler } = require('../utils');
-const { loginValidators, validationResult } = require('./validations')
+const { loginValidators, signupValidators, validationResult } = require('./validations')
 const { loginUser } = require('../auth')
 
 var router = express.Router();
@@ -15,11 +15,11 @@ router.get('/', asyncHandler( async (req, res, next) => {
 }));
 
 router.get('/sign-up', csrfProtection, asyncHandler(async (req, res) => {
-
-  res.render('sign-up', {csrfToken: req.csrfToken()});
+  const user = User.build(); //double check with team
+  res.render('sign-up', { user, csrfToken: req.csrfToken()});
 }));
 
-router.post('/sign-up', csrfProtection, asyncHandler(async(req, res) => {
+router.post('/sign-up', csrfProtection, signupValidators, asyncHandler(async(req, res) => {
   const { firstName, lastName, email, password, avatarUrl, shortBio } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -33,8 +33,23 @@ router.post('/sign-up', csrfProtection, asyncHandler(async(req, res) => {
     shortBio
    });
 
-  res.redirect('/users');
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    await user.save();
+    res.redirect('/users');
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('sign-up', {
+      user,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  }
 }));
+
+//   res.redirect('/users');
+// }));
 
 
 router.get('/log-in', csrfProtection, asyncHandler(async(req, res) => {
@@ -43,7 +58,7 @@ router.get('/log-in', csrfProtection, asyncHandler(async(req, res) => {
 
 router.post('/log-in', loginValidators, csrfProtection, asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-  
+
     let errors = [];
     const validatorErrors = validationResult(req);
     console.log(validatorErrors)
@@ -66,4 +81,10 @@ router.post('/log-in', loginValidators, csrfProtection, asyncHandler(async (req,
     res.render('log-in', { email, errors, csrfToken: req.csrfToken() });
 
   }));
+
+  router.post('/logout', (req, res) => { //double check url with team
+    logoutUser(req, res);
+    res.redirect('/'); //double check where we want redirect
+  });
+
 module.exports = router;
