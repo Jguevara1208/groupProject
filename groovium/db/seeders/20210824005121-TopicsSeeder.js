395f4a8fe602efcffad1ef78e4b2006093680b79
aivnerrad <aivnerrad1993@gmail.com>
2021-08-24 00:43:19 -0400

    finished seeder files

--- groovium/db/seeders/20210824005121-TopicsSeeder.js
+++ groovium/db/seeders/20210824005121-TopicsSeeder.js
@@ -2,7 +2,12 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-   return queryInterface.bulkInsert('Topics', [], {});
+   return queryInterface.bulkInsert('Topics', [
+    { topic: "Food", createdAt: '2021-08-21', updatedAt: '2021-08-22'},
+    { topic: "Pets", createdAt: '2021-08-21', updatedAt: '2021-08-22'},
+    { topic: "Sports", createdAt: '2021-08-21', updatedAt: '2021-08-22'},
+    { topic: "Tech", createdAt: '2021-08-21', updatedAt: '2021-08-22'}
+   ], {});
   },
 
   down: (queryInterface, Sequelize) => {
