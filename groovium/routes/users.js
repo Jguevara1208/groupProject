b0f31cc014ec1c42a7184c7b2d2904cecde8670f
Peter Joh <johpeter@gmail.com>
2021-08-24 19:56:00 -0600

    worked out routes now need pug

--- groovium/routes/users.js
+++ groovium/routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require("../utils")
+const { Story, User, Topic } = require("../db/models");
+
 
 router.get('/', asyncHandler(async (req, res) => {
     res.render('home')
 }))
+
+router.get('/:userId', asyncHandler(async (req, res) => {
+    const userId = req.url.split("/")[1];
+    const user = await User.findByPk(userId);
+    const story = await Story.findAll({
+        where: {
+            userId: userId
+        }
+    });
+    const topic = await Topic.findAll();
+
+    console.log(user, story, topic)
+    res.render('user-profile-page', { story, user, topic })
+  }));
 
 module.exports = router;
