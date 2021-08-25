6f304b7e96db5b5ca0fbf5609e5f2b66da68a27a
aivnerrad <aivnerrad1993@gmail.com>
2021-08-25 01:15:07 -0400

    pause

--- groovium/routes/topics.js
+++ groovium/routes/topics.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require("../utils")
-const { Topic } = require("../db/models");
+const { Topic, Story, User } = require("../db/models");
+
 
 router.get('/', asyncHandler(async (req, res) => {
     const topics = await Topic.findAll()
@@ -11,7 +12,17 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/:topicId', asyncHandler(async (req, res) => {
     const topicId = req.url.split("/")[1]
     const topic = await Topic.findByPk(topicId)
-    console.log(topic.imageUrl)
-    res.render('topic-page', { topic })
+    const stories = await Story.findAll({
+        where: {
+            topicId
+        }
+    })
+    const followers = await User.findAll({
+        include: {
+            model: Topic,
+            as: "topicId"
+    }})
+    console.log(followers, "<----- followers")
+    res.render('topic-page', { topic, stories })
   }));
 module.exports = router;
