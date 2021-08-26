887b32d0e2a1be313d7a9eee9f43fbd17097edc7
Jordan <jordansacct@gmail.com>
2021-08-26 11:17:05 -0700

    hope this works

--- routes/stories.js
+++ routes/stories.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require("../utils")
-const { Story, User, Topic } = require("../db/models");
+const { Story, User, Topic, Comment } = require("../db/models");
 
 router.get('/:storyId', asyncHandler(async (req, res) => {
-  const storyId = req.url.split("/")[1]
+  const storyId = req.params.storyId
   const story = await Story.findByPk(storyId)
   const user = await User.findByPk(story.userId)
   const topic = await Topic.findByPk(story.topicId)
+  const comments = await Comment.findAll({
+    where: {
+      storyId
+    }
+  })
+
+  const commentNumber = comments.length
+
+  console.log(commentNumber)
+  console.log(comments)
   // console.log("THE STORY --------> ", story)
   // console.log("THE USER --------> ", user)
   // console.log("THE TOPIC --------> ", topic)
-  res.render('stories', { story, user, topic })
+  res.render('stories', { story, user, topic, comments, commentNumber})
 }));
 
 module.exports = router;
