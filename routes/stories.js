76d9690ee22973323398a9e44a7c9398ff7df5d9
Blair Wishom <blwishom@gmail.com>
2021-08-26 22:16:04 -0700

    working on edit stories

--- routes/stories.js
+++ routes/stories.js
@@ -37,4 +37,47 @@ router.get('/:storyId', asyncHandler(async (req, res) => {
   res.render('stories', { userId, story, user, topic, comments, commentNumber})
 }));
 
+//Edit
+router.get('/:storyId/edit', asyncHandler(async (req, res) => {
+  const storyId = req.url.split('/')[1];
+  const userId = req.session.auth.userId;
+  const story = await Story.findByPk(storyId)
+  const topics = await Topic.findAll()
+  // console.log(topics)
+  res.render('edit-story', { topics, story })
+}));
+
+router.post('/my-stories/edit', asyncHandler(async(req, res) => {
+
+  const { title, body, storyImgUrl, topicId } = req.body;
+  const userId = req.session.auth.userId
+
+  console.log('-------------------------------------------------')
+  console.log(req.body)
+  console.log(userId)
+
+  console.log(body, "--------------------------------")
+  console.log(typeof(body))
+
+  const summary = body.slice(0, 100)
+
+  const bodysize = body.length
+  const readTimeMinutes = Math.floor(bodysize/190)
+
+  //change to build and save later after validations
+  const post = await Story.create({
+      userId,
+      topicId,
+      summary,
+      title,
+      readTimeMinutes,
+      body,
+      storyImgUrl
+  });
+
+
+  res.redirect('/users/my-stories')
+
+}));
+
 module.exports = router;
