9a51392c8ca9682a319d984c8c23e42534b6f2db
Peter Joh <johpeter@gmail.com>
2021-08-29 02:23:29 -0600

    front page slide thing and banner fixed to be dynamic

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,15 @@ var router = express.Router();
 router.get('/', asyncHandler( async (req, res) => {
   const stories = await Story.findAll({
     include: [User, Topic],
-    limit: 6
+    limit: 20
   })
   const newStories = stories.map(story => {
     const date = story.createdAt
     const month = date.getMonth() + 1
     const day = date.getDate()
     const newDate = `${month}-${day}`
+    let longsum = story.body.slice(0, 814).concat('......')
+
 
     return {
       id: story.id,
@@ -28,7 +30,7 @@ router.get('/', asyncHandler( async (req, res) => {
       avatarUrl: story.User.avatarUrl,
       firstName: story.User.firstName,
       lastName: story.User.lastName,
-      summary: story.summary,
+      summary: longsum,
       date: newDate,
       readTimeMinutes: story.readTimeMinutes,
       topicId: story.topicId,
@@ -105,7 +107,7 @@ router.post('/log-in', csrfProtection, loginValidators, asyncHandler(async (req,
     errors.push('Login failed for the provided email address and password');
   } else {
     errors = validatorErrors.array().map((error) => {
-      return { error: error.msg, field: error.param } 
+      return { error: error.msg, field: error.param }
     });
   }
 
@@ -123,4 +125,37 @@ router.get('/demo', csrfProtection, asyncHandler( async (req,res) => {
   loginUser(req, res, demoUser)
   return req.session.save(() => res.redirect('/users'));
 }))
+
+// router.get('/test', asyncHandler( async (req, res) => {
+//   const stories = await Story.findAll({
+//     include: [User, Topic],
+//     limit: 20
+//   })
+//   const newStories = stories.map(story => {
+//     const date = story.createdAt
+//     const month = date.getMonth() + 1
+//     const day = date.getDate()
+//     const newDate = `${month}-${day}`
+//     let longsum = story.body.slice(0, 814).concat('......')
+
+//     return {
+//       id: story.id,
+//       title: story.title,
+//       userId: story.User.id,
+//       avatarUrl: story.User.avatarUrl,
+//       firstName: story.User.firstName,
+//       lastName: story.User.lastName,
+//       summary: longsum,
+//       body: story.body,
+//       date: newDate,
+//       readTimeMinutes: story.readTimeMinutes,
+//       topicId: story.topicId,
+//       topic: story.Topic.topic,
+//       storyImgUrl: story.storyImgUrl
+//     }
+//   })
+
+//   res.render('test-view', { newStories });
+// }));
+
 module.exports = router;
